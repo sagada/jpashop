0a2dc93787d299bcf230d5b67d0de87a4ef2f11d
sagada <wlatmdqja@naver.com>
2020-11-23 01:00:57 +0900

    웹 구현

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -12,18 +12,13 @@ public class OrderRepository {
 
     private final EntityManager em;
 
-    public void save(Order order)
-    {
+    public void save(Order order) {
         em.persist(order);
     }
 
-    public Order findOne(Long id)
-    {
+    public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll()
-//    {
-//        return em.
-//    }
+
 }
